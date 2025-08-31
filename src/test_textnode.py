34292af95236894b3fa9caf55dc427859065392f
kitanaya <seb.heinzmann@gmail.com>
2025-08-31 19:14:32 +0200

    Nodes L5 + L6 missing testcases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -33,6 +33,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("different text_type", TextType.IMAGE)
         node2 = TextNode("different text_type", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
