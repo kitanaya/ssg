f7706cebbdd700a843190b141a1e1ec115b7b730
kitanaya <seb.heinzmann@gmail.com>
2025-09-06 12:51:48 +0200

    Ch3 L6

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -6,6 +6,7 @@ from inline_markdown import (
     extract_markdown_images,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -270,6 +271,26 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode("b", TextType.IMAGE, "u2"),
             ],
             nodes_after_image,
+        )
+
+    def test_text_to_textnode(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)", 
+        )
+        self.assertEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
         )
 
 if __name__ == "__main__":
