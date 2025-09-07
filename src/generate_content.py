9a0920ca94b2e3ceb77930a7209ae114bbe759db
kitanaya <seb.heinzmann@gmail.com>
2025-09-07 16:18:30 +0200

    got help from gpt5, very nice.

--- src/generate_content.py
+++ src/generate_content.py
@@ -14,15 +14,13 @@ def extract_title(markdown):
 	raise Exception("no h1 Title found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 	print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-	from_file = open(from_path, "r")
-	markdown_content = from_file.read()
-	from_file.close()
+	with open(from_path, "r") as f:
+		markdown_content = f.read()
 
-	template_file = open(template_path, "r")
-	template_content = template_file.read()
-	template_file.close()
+	with open(template_path, "r") as f:
+		template_content = f.read()
 
 	node = markdown_to_html_node(markdown_content)
 	html_content = node.to_html()
@@ -31,20 +29,20 @@ def generate_page(from_path, template_path, dest_path):
 	template_content = template_content.replace("{{ Title }}", title)
 	template_content = template_content.replace("{{ Content }}", html_content)
 
+	template_content = template_content.replace('href="/', f'href="{basepath}')
+	template_content = template_content.replace('src="/', f'src="{basepath}')
 
-	dest_dir_path = os.path.dirname(dest_path)
-	if dest_dir_path != "":
-		os.makedirs(dest_dir_path, exist_ok=True)
-	write_content_to_file = open(dest_path, "w")
-	write_content_to_file.write(template_content)
+	os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+	with open(dest_path, "w") as f:
+		f.write(template_content)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
 	for filename in os.listdir(dir_path_content):
 		from_path = os.path.join(dir_path_content, filename)
 		dest_path = os.path.join(dest_dir_path, filename)
 		if os.path.isfile(from_path):
 			dest_path = Path(dest_path).with_suffix(".html")
-			generate_page(from_path, template_path, dest_path)
+			generate_page(from_path, template_path, dest_path, basepath)
 		else:
-			generate_pages_recursive(from_path, template_path, dest_path)
+			generate_pages_recursive(from_path, template_path, dest_path, basepath)
