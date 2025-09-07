8a85477fa6c809660a4ac98bd5bc964e0c44e3a6
kitanaya <seb.heinzmann@gmail.com>
2025-09-07 10:43:19 +0200

    This was impossible without boots and even peeking at the solution :'(

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node, TextNode, TextType
+
 class BlockType(Enum):
 	PARAGRAPH = "paragraph"
 	HEADING = "heading"
@@ -39,3 +43,90 @@ def block_to_block_type(block):
 		return BlockType.ORDERED_LIST
 
 	return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+	blocks = markdown_to_blocks(markdown)
+	children = []
+	for block in blocks:
+		html_node = block_to_html_node(block)
+		children.append(html_node)
+	return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+	block_type = block_to_block_type(block)
+	if block_type == BlockType.PARAGRAPH:
+		return paragraph_to_html_node(block)
+	if block_type == BlockType.HEADING:
+		return heading_to_html_node(block)
+	if block_type == BlockType.CODE:
+		return code_to_html_node(block)
+	if block_type == BlockType.QUOTE:
+		return quote_to_html_node(block)
+	if block_type == BlockType.UNORDERED_LIST:
+		return unordered_list_to_html_node(block)
+	if block_type == BlockType.ORDERED_LIST:
+		return ordered_list_to_html_node(block)
+	raise ValueError("invalid block type")
+
+
+def text_to_children(text):
+	text_nodes = text_to_textnodes(text)
+	children = []
+	for text_node in text_nodes:
+		html_node = text_node_to_html_node(text_node)
+		children.append(html_node)
+	return children
+
+
+def heading_to_html_node(block):
+	level = len(block.split(" ", 1)[0])
+	text = block[level + 1:].rstrip()
+	children = text_to_children(text)
+	return ParentNode(f"h{level}", children, None)
+
+
+def paragraph_to_html_node(block):
+	lines = block.split("\n")
+	text = " ".join(lines)
+	children = text_to_children(text)
+	return ParentNode("p", children, None)
+
+
+def code_to_html_node(block):
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+
+
+def quote_to_html_node(block):
+	lines = block.split("\n")
+	new_lines = []
+	for line in lines:
+		new_lines.append(line.lstrip(">").strip())
+	content = " ".join(new_lines)
+	children = text_to_children(content)
+	return ParentNode("blockquote", children)
+
+
+def unordered_list_to_html_node(block):
+	items = block.split("\n")
+	html_items = []
+	for item in items:
+		text = item[2:]
+		children = text_to_children(text)
+		html_items.append(ParentNode("li", children))
+	return ParentNode("ul", html_items)
+
+
+def ordered_list_to_html_node(block):
+	items = block.split("\n")
+	html_items = []
+	for item in items:
+		text = item[3:]
+		children = text_to_children(text)
+		html_items.append(ParentNode("li", children))
+	return ParentNode("ol", html_items)
